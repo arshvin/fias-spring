d8694b1d9599dc10612dd2d7e4ea22f817bf5cf7
Arshvin <arschvin@gmail.com>
2015-10-16 00:04:45 +0500

    Added and tested schema validator

---
+++ src/test/java/medved/TestValidators.java
@@ -0,0 +1,32 @@
+package medved;
+
+import medved.valiadators.SchemaValidator;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by arshvin on 15.10.15.
+ */
+//@Ignore("Disactivated temporally for quikness...")
+public class TestValidators {
+
+    @Test
+    public void testAddrObjXml() throws IOException, SAXException {
+        SchemaValidator validator = new SchemaValidator(
+                ClassLoader.getSystemResource("AS_ADDROBJ_2_250_01_04_01_01.xsd").getPath(),
+                ClassLoader.getSystemResource("AS_ADDROBJ_TEST_forCreate.xml").getPath());
+        assertTrue(validator.examine());
+    }
+
+    @Test
+    public void testHouseXml() throws IOException, SAXException {
+        SchemaValidator validator = new SchemaValidator(
+                ClassLoader.getSystemResource("AS_HOUSE_2_250_02_04_01_01.xsd").getPath(),
+                ClassLoader.getSystemResource("AS_HOUSE_TEST_forCreate.xml").getPath());
+        assertTrue(validator.examine());
+    }
+}
