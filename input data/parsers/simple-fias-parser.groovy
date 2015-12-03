9933cc7e9abdcdb18fbc9750c4cbbc3efbcd565e
Arshvin <arschvin@gmail.com>
2015-12-03 09:18:15 +0500

    Done another yet parser for generate tests data

--- input data/parsers/simple-fias-parser.groovy
+++ input data/parsers/simple-fias-parser.groovy
@@ -15,20 +15,29 @@ def objAttribList = ("AOID AOGUID PARENTGUID NEXTID FORMALNAME OFFNAME SHORTNAME
 def houseAttribList = ("HOUSEID HOUSEGUID AOGUID HOUSENUM STRSTATUS ESTSTATUS STATSTATUS POSTALCODE " +
                         "STARTDATE ENDDATE UPDATEDATE COUNTER").split(" ")
 
+def fields = "AOID AOGUID PARENTGUID NEXTID FORMALNAME OFFNAME POSTALCODE ".split(" ")
 File input = new File(args[0])
 File output = new File(args[1])
-output.text = ""
 
 XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance()
 XMLStreamReader xmlStreamReader = xmlInputFactory.createXMLStreamReader(new FileReader(input))
 
 xmlStreamReader.nextTag()
-xmlStreamReader.nextTag()
 
-while (xmlStreamReader.getEventType() == XMLStreamConstants.START_ELEMENT){
-    while (xmlStreamReader.getEventType() != XMLStreamConstants.END_ELEMENT) {
-        xmlStreamReader.next()
+output.withWriter{ Writer writer ->
+    while (xmlStreamReader.getEventType() != XMLStreamConstants.END_DOCUMENT){
+        if (xmlStreamReader.getEventType() == XMLStreamConstants.START_ELEMENT){
+            def outLine = new StringBuilder()
+
+            0.upto(xmlStreamReader.getAttributeCount()){index ->
+                if (xmlStreamReader.getAttributeLocalName(index) in fields){
+                    outLine.append("${xmlStreamReader.getAttributeLocalName(index)}:${xmlStreamReader.getAttributeValue(index)} ")
+                }
+            }
+            println outLine.toString()
+            writer <<"${outLine.toString()}\n"
+        }
+
+        xmlStreamReader.nextTag()
     }
-    output <<"${outLine.toString()}\n"
-    xmlStreamReader.nextTag()
 }
--- input data/parsers/simple-fias-sawer.groovy
+++ input data/parsers/simple-fias-sawer.groovy
@@ -14,7 +14,35 @@ File output = new File(args[1])
 XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance()
 XMLStreamReader xmlStreamReader = xmlInputFactory.createXMLStreamReader(new FileInputStream(input), "utf-8")
 
+level = 3
+counter = 100
+hierchyLevel = [1:new HashSet()]
+
+putToHierchy = {aoGuid, parentGuid ->
+    levels = hierchyLevel.collect{key,value -> key}.max()
+    if (!parentGuid) {//first level of hierchy
+        hierchyLevel[1].add(aoGuid)
+        println "aoGuid $aoGuid added at level 1"
+        1
+    } else {
+        result = 0
+        for(l=1;l<levels+1;l++)each{
+            if (hierchyLevel[l].contains(parentGuid)){
+                if (!hierchyLevel.containsKey(l+1)){
+                    hierchyLevel[l+1] = new HashSet()
+                }
+                hierchyLevel[l+1].add(aoGuid)
+                println "aoGuid $aoGuid added at level ${l+1}"
+
+                result = l+1
+            }
+        }
+        result
+    }
+}
+
 output.withWriter{ Writer writer ->
+
     if (xmlStreamReader.getEventType() == START_DOCUMENT) {
         writer << "<?xml version=${xmlStreamReader.getVersion()} encoding=${xmlStreamReader.getEncoding()}?>\n"
     }
@@ -22,27 +50,44 @@ output.withWriter{ Writer writer ->
     container = xmlStreamReader.getLocalName()
     writer << "<$container>\n"
     xmlStreamReader.nextTag()
-    ii=0
+
     while (xmlStreamReader.getEventType() != END_DOCUMENT){
-        StringBuilder stringBuilder = new StringBuilder()
         if (xmlStreamReader.getEventType() == START_ELEMENT){
+            StringBuilder stringBuilder = new StringBuilder()
             stringBuilder.append("<${xmlStreamReader.getLocalName()} ")
 
+            aoGuid = parentGuid = ""
             for (i=0; i < xmlStreamReader.getAttributeCount(); i++){
                 stringBuilder.append("${xmlStreamReader.getAttributeLocalName(i)}=\"${xmlStreamReader.getAttributeValue(i)}\" ")
+
+                switch (xmlStreamReader.getAttributeLocalName(i).toUpperCase()){
+                    case "PARENTGUID":
+                        parentGuid = xmlStreamReader.getAttributeValue(i)
+                        break
+                    case "AOGUID":
+                        aoGuid = xmlStreamReader.getAttributeValue(i)
+                }
             }
 
-            stringBuilder.append("/>\n")
-            xmlStreamReader.nextTag()
+            currentLevel = putToHierchy(aoGuid,parentGuid)
+            if (currentLevel > 0){
+                stringBuilder.append("/>\n")
+                writer << stringBuilder.toString()
+
+                if (currentLevel == level){
+                    counter--
+                }
+            }
+        } else if (xmlStreamReader.getEventType() == END_ELEMENT){
+            writer << "</${xmlStreamReader.getLocalName()}>\n"
         }
-        writer << stringBuilder.toString()
+
+        xmlStreamReader.nextTag()
         xmlStreamReader.nextTag()
 
-        if (ii<-1){
-            writer << "<$container/>\n"
+        if (counter == 0){
+            writer << "</$container>\n"
             break
-        } else {
-            ii++
         }
     }
 
